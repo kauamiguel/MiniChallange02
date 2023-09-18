a2b4408e1c0c8ae20033b42d5d9de45b6ea10bb5
Daniel Ishida <ishidadecol@gmail.com>
2023-09-18 15:27:38 -0300

    Created View and ViewController classes

---
+++ Mini02/Mini02/Appointments/ApointmentsList/AppointementsListVC.swift
@@ -0,0 +1,18 @@
+//
+//  AppointementsListVC.swift
+//  Mini02
+//
+//  Created by Daniel Ishida on 18/09/23.
+//
+
+import UIKit
+
+class AppointementsListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ Mini02/Mini02/Appointments/ApointmentsList/AppointmentListView.swift
@@ -0,0 +1,21 @@
+//
+//  AppointmentListView.swift
+//  Mini02
+//
+//  Created by Daniel Ishida on 18/09/23.
+//
+
+import UIKit
+
+class AppointmentListView: UITableViewCell {
+    
+    //UI SETUP
+    func setupList(vc:AppointementsListVC){
+        self.backgroundColor = .systemBackground
+        
+    }
+    
+    
+    //CONSTRAINTS SET UP
+
+}
