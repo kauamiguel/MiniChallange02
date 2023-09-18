5ff0bd165be062e331456bc398013a89c3fb75d4
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-09-18 09:54:45 -0300

    Update SceneDelegate.swift

--- Mini02/Mini02/Delegate/SceneDelegate.swift
+++ Mini02/Mini02/Delegate/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let window = UIWindow(windowScene: windowScene)
         window.makeKeyAndVisible()
-        window.rootViewController = RegisterViewController()
+        window.rootViewController = TabBarViewController()
         
         self.window = window
     }
