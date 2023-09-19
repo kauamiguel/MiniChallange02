128498a5275ace0934fb554e723bf6fe917eba34
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-09-19 13:58:39 -0300

    não utilizando

--- Mini02/Mini02/Profile/ProfileView.swift
+++ Mini02/Mini02/Profile/ProfileView.swift
@@ -10,60 +10,7 @@ import UIKit
 class ProfileView: UIView {
     
     
-    
-    // Add subviews and configure them in your custom view
-    
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Custom View Title"
-        label.textColor = .black
-        label.font = UIFont.boldSystemFont(ofSize: 18)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let descriptionLabel: UILabel = {
-        let label = UILabel()
-        label.text = "This is a custom view."
-        label.textColor = .gray
-        label.font = UIFont.systemFont(ofSize: 14)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    // Custom view initialization
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupUI()
-        print("cleber")
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setupUI()
-    }
-    
-    // Setup and layout subviews
-    
-    private func setupUI() {
-        backgroundColor = .white
-        
-        // Add subviews to the custom view
-        addSubview(titleLabel)
-        addSubview(descriptionLabel)
-        
-        // Define constraints for subviews
-        NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            
-            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
-            descriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            descriptionLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            descriptionLabel.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -16)
-        ])
-    }
+  
 }
 
 
