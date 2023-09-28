3b66f96f1f15b41f65c90ac429f4cfa1943d0dec
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-09-28 15:40:16 -0300

    .

---
+++ Mini02/Mini02/Appointments/AppointmentList/Histórico /HistoryView.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryView.swift
+//  Mini02
+//
+//  Created by Gabriel Eirado on 28/09/23.
+//
+
+import UIKit
+
+class HistoryView: UIView {
+
+    func setupView(){
+        
+        self.backgroundColor = .red
+        
+    }
+}
