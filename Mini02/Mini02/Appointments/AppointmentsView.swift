54c9c58271f996f1f0d40b52dcaf88897d6e313e
kauamiguel <kauamcm@gmail.com>
2023-09-29 09:30:43 -0300

    Adicionando o desenho da tela inicial

--- Mini02/Mini02/Appointments/AppointmentsView.swift
+++ Mini02/Mini02/Appointments/AppointmentsView.swift
@@ -26,7 +26,7 @@ class AppointmentsView: UIView {
         button.backgroundColor = .gray
         button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Primeiro trimestre", for: .normal)
+        button.setTitle("1", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         return button
     }()
@@ -38,7 +38,7 @@ class AppointmentsView: UIView {
         button.backgroundColor = .gray
         button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Segundo trimestre", for: .normal)
+        button.setTitle("2", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         return button
     }()
@@ -50,7 +50,7 @@ class AppointmentsView: UIView {
         button.backgroundColor = .gray
         button.layer.cornerRadius = 10
         button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Terceiro trimestre", for: .normal)
+        button.setTitle("3", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         return button
     }()
@@ -84,20 +84,18 @@ class AppointmentsView: UIView {
         thirdTremesterButton.addTarget(vc, action: action, for: .touchUpInside)
         
         //Setting up the layout of the view
-        vc.view.addSubview(Vstack)
-        Vstack.addArrangedSubview(firstTremesterButton)
-        Vstack.addArrangedSubview(secondTremesterButton)
-        Vstack.addArrangedSubview(thirdTremesterButton)
+        vc.view.addSubview(firstTremesterButton)
+        vc.view.addSubview(secondTremesterButton)
+        vc.view.addSubview(thirdTremesterButton)
         vc.view.addSubview(userName)
         vc.view.addSubview(userInfo)
         
-        //Setup constrains of each view
-        Vstack.centerX(inView: vc.view)
-        Vstack.centerY(inView: vc.view)
         
-        firstTremesterButton.anchorWithConstantValues(width: 200, height: 80)
-        secondTremesterButton.anchorWithConstantValues(width: 200, height: 80)
-        thirdTremesterButton.anchorWithConstantValues(width: 200, height: 80)
+        //Adding constraints
+        
+        firstTremesterButton.anchorWithConstantValues(width: 100, height: 80)
+        secondTremesterButton.anchorWithConstantValues(width: 100, height: 80)
+        thirdTremesterButton.anchorWithConstantValues(width: 100, height: 80)
         
         let profileButton = ProfileImageButton(controller: vc)
         vc.view.addSubview(profileButton)
@@ -108,6 +106,82 @@ class AppointmentsView: UIView {
         
         userInfo.centerX(inView: vc.view)
         userInfo.anchorWithConstantValues(top: userName.bottomAnchor, topPadding: 5)
+        
+        firstTremesterButton.anchorWithConstantValues(top: userInfo.bottomAnchor,left: vc.view.leadingAnchor ,topPadding: 40, leftPadding: 30)
+        secondTremesterButton.centerY(inView: vc.view)
+        secondTremesterButton.anchorWithConstantValues(right: vc.view.trailingAnchor, rightPadding: -20)
+        thirdTremesterButton.anchorWithConstantValues(left: vc.view.leadingAnchor, bottom: vc.view.bottomAnchor, leftPadding: 50, bottomPadding: -100)
+        
+        
+    }
+    
+    func drawDashedCurvedLineBetweenButtons(vc : AppointmentsViewController ,isFirstTremesteer : Bool, isSecondTremester : Bool) {
+        
+        //Create the main points of the curve calculus
+        let startPoint = CGPoint(x: firstTremesterButton.frame.maxX, y: firstTremesterButton.center.y)
+        let endPoint = CGPoint(x: secondTremesterButton.center.x, y: secondTremesterButton.frame.minY)
+        let midPoint = CGPoint(x: (startPoint.x + endPoint.x) / 2, y: (startPoint.y + endPoint.y) / 2)
+        
+        //Create the 2 points that will help to control the curve
+        let controlPoint1 = CGPoint(x: (startPoint.x + midPoint.x) - 200 , y: (startPoint.y + midPoint.y) / 3)
+        let controlPoint2 = CGPoint(x: (endPoint.x + midPoint.x)  - 200, y: (endPoint.y + midPoint.y) / 3)
+        
+        //Create the path based on that point
+        let path = UIBezierPath()
+        path.move(to: startPoint)
+        path.addCurve(to: endPoint, controlPoint1: controlPoint1, controlPoint2: controlPoint2)
+        
+        //Draw the path
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        
+        if !isFirstTremesteer{
+            shapeLayer.opacity = 0.2
+        }else{
+            shapeLayer.opacity = 1
+        }
+        
+        shapeLayer.lineWidth = 15
+        shapeLayer.lineDashPattern = [10, 30]
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = nil
+        shapeLayer.lineCap = .round
+        
+        //Insert in the bottom of the subviews stack
+        vc.view.layer.insertSublayer(shapeLayer, at: 0)
+        
+        
+        //Create the points between the middele button and the third button
+        let startPointMidButton = CGPoint(x: secondTremesterButton.frame.minX, y: secondTremesterButton.center.y)
+        let endPointThirdButton = CGPoint(x: thirdTremesterButton.center.x, y: thirdTremesterButton.center.y)
+        let midPointSecondCurve = CGPoint(x: (startPointMidButton.x + endPointThirdButton.x) / 2, y: (startPointMidButton.y + endPointThirdButton.y) / 2)
+        
+        //Create the main point of the curve
+        let controlPoint3 = CGPoint(x: (startPointMidButton.x - midPointSecondCurve.x) / 8 , y: (startPointMidButton.y - midPointSecondCurve.y) + 500)
+        let controlPoint4 = CGPoint(x: (endPointThirdButton.x - midPointSecondCurve.x) / 8 , y: (endPointThirdButton.y - midPointSecondCurve.y) + 500)
+        
+        //Create the path
+        let secondPath = UIBezierPath()
+        secondPath.move(to: startPointMidButton)
+        secondPath.addCurve(to: endPointThirdButton, controlPoint1: controlPoint3, controlPoint2: controlPoint4)
+        
+        //Draw the path
+        let shapeLayer2 = CAShapeLayer()
+        shapeLayer2.strokeColor = UIColor.red.cgColor
+        
+        if !isSecondTremester{
+            shapeLayer2.opacity = 0.2
+        }else{
+            shapeLayer2.opacity = 1
+        }
+        
+        shapeLayer2.lineWidth = 15
+        shapeLayer2.lineDashPattern = [10, 30]
+        shapeLayer2.path = secondPath.cgPath
+        shapeLayer2.fillColor = nil
+        shapeLayer2.lineCap = .round
+        
+        vc.view.layer.insertSublayer(shapeLayer2, at: 1)
     }
 }
 
