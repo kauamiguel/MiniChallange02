6580d0ca4a722d76e850ad0cd94de52047669bea
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-09-29 14:51:28 -0300

    .

--- Mini02/Mini02/Appointments/MaternityCard/MaternityCardViewController.swift
+++ Mini02/Mini02/Appointments/MaternityCard/MaternityCardViewController.swift
@@ -38,9 +38,7 @@ class MaternityCardViewController: UICollectionViewController, UICollectionViewD
             CellInfo(view: plannedView, size: plannedView.pregnancyRiskViewSize, id: PlannedView.id),
             CellInfo(view: currentGestationView, size: currentGestationView.currentGestationViewSize, id: CurrentGestationView.id),
             CellInfo(view: clinicAntecedentsView, size: clinicAntecedentsView.clinicAntecedentsViewSize, id: ClinicAntecedentsView.id)
-            //            CellInfo(view: bloodView, size: bloodView.bloodViewViewSize, id: BloodView.id),
-            
-            //CellInfo(view: ultrasoundView, size: ultrasoundView.ultrasoundViewSize, id: UltrasoundView.id)
+          
         ]
         
         self.collectionView.dataSource = self
