55bbdb748241ba236529961e1e63edbccf8fecf1
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-10-03 14:59:54 -0300

    Update FooterCell.swift

--- Mini02/Mini02/Appointments/MaternityCard/ComponetViews/routine appointment/FooterCell.swift
+++ Mini02/Mini02/Appointments/MaternityCard/ComponetViews/routine appointment/FooterCell.swift
@@ -24,6 +24,40 @@ public class FooterCell: UICollectionReusableView {
         return btn
     }()
     
+    let saveButton: UIButton = {
+        let btn = UIButton()
+        
+        // Set the background color and corner radius to make it a rounded rectangle
+        btn.backgroundColor = UIColor(red: 1, green: 0.521, blue: 0.58, alpha: 1)
+        btn.layer.cornerRadius = 8 // Adjust the corner radius to your preference
+        
+        
+        var btnConfig = UIButton.Configuration.borderless()
+        btnConfig.titleAlignment = .trailing
+        btnConfig.imagePlacement = .leading
+    
+        btnConfig.titleLineBreakMode = .byWordWrapping
+        btn.tintColor = UIColor(red: 1, green: 0.521, blue: 0.58, alpha: 1)
+        // Create a template image so it scales without distortion
+        if let image = UIImage(systemName: "bookmark")?.withRenderingMode(.automatic) {
+            btnConfig.image = image
+        }
+
+        
+        btn.configuration = btnConfig
+
+        
+        
+        // Set the image tint color
+        btn.tintColor = .white
+        
+        btn.setTitle("Save", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "YourFontName", size: 16)
+        
+        return btn
+    }()
+    
     let label = LabelComponentView()
     
     lazy var addHStack: UIStackView = {
@@ -57,6 +91,9 @@ public class FooterCell: UICollectionReusableView {
          addHStack.addArrangedSubview(plusButton)
          addHStack.addArrangedSubview(label)
          addHStack.anchorWithConstantValues(top: self.topAnchor, left: self.leadingAnchor, topPadding: 15, leftPadding: 15)
+         
+         self.addSubview(saveButton)
+         saveButton.anchorWithConstantValues(top: addHStack.bottomAnchor, left: self.leadingAnchor ,leftPadding: 15, width: UIScreen.main.bounds.width * 0.46, height: UIScreen.main.bounds.height * 0.035)
          
     }
 
