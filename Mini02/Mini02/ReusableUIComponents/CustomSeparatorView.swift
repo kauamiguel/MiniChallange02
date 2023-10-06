cd8921fd680ccd300534be17772d683e45a81431
Eirado <GABEIRADO2@HOTMAIL.COm>
2023-10-06 12:30:37 -0300

    .

---
+++ Mini02/Mini02/ReusableUIComponents/CustomSeparatorView.swift
@@ -0,0 +1,26 @@
+//
+//  CustomSeparator.swift
+//  Mini02
+//
+//  Created by Gabriel Eirado on 06/10/23.
+//
+
+import UIKit
+
+class CustomSeparatorView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+        self.backgroundColor = UIColor(red: 0.70, green: 0.82, blue: 0.84, alpha: 1.00)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+    }
+    
+}
+
+
+
+
