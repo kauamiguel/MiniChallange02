837b355e0f5c87181af4dccda7499eae783ae413
Eirado <GABEIRADO2@HOTMAIL.COm>
2024-03-09 17:46:27 -0300

    trying to fix TextField

---
+++ Mini02/Mini02/Appointments/MaternityCard/ComponetViews/TextField/TextFieldComponent.swift
@@ -0,0 +1,69 @@
+//
+//  TextFieldComponent.swift
+//  Mini02
+//
+//  Created by Gabriel Eirado on 09/03/24.
+//
+
+import UIKit
+
+class TextViewComponet: UITextView, UITextViewDelegate {
+    let placeholderText = "Digite suas notas aqui"
+    var isPlaceholderShown = true
+    var typedText: String?
+        
+    
+    func setupTextView(){
+        
+        
+        self.backgroundColor = .blue
+        self.returnKeyType = .done
+        self.autocapitalizationType = .sentences
+        self.isScrollEnabled = true
+        self.showsVerticalScrollIndicator = false
+        self.autocorrectionType = .default
+        
+        let bodyFont = UIFont.preferredFont(forTextStyle: .body)
+        let scaledFontSize = bodyFont.pointSize * 1.1
+        let signikaBoldFont = UIFont(name: "Signika-Bold", size: scaledFontSize)
+        
+        self.font = signikaBoldFont
+        
+        self.delegate = self
+        
+        
+        self.delegate = self
+        if typedText != nil {
+            isPlaceholderShown = false
+        }
+        self.text = typedText ?? placeholderText
+        self.textColor = .lightGray
+
+       
+    }
+    
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if isPlaceholderShown {
+            textView.text = nil
+            textView.textColor = UIColor.darkPink()
+            isPlaceholderShown = false
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = placeholderText
+            textView.textColor = .lightGray
+            isPlaceholderShown = true
+        }
+    }
+    func textViewDidChange(_ textView: UITextView) {
+        typedText = textView.text // Update typedText property when text changes
+    }
+    
+    func getTypedText() -> String? {
+        return typedText // Function to return the typed text
+    }
+}
+
--- Mini02/Mini02/Appointments/MaternityCard/ComponetViews/TextField/TextFieldView.swift
+++ Mini02/Mini02/Appointments/MaternityCard/ComponetViews/TextField/TextFieldView.swift
@@ -1,33 +1,17 @@
 import UIKit
 
-class TextView: UIView, UITextViewDelegate{
+class TextView: UIView{
     
     static let id = "TextViewCell"
     let query = "textView"
     
     let screenSize = CGSize(width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
     let viewSize = CGSize(width: trunc((UIScreen.main.bounds.size.width - UIScreen.main.bounds.size.width * 0.04)), height: UIScreen.main.bounds.size.height * 0.34)
-    
-    private let placeholderText = "Digite suas notas aqui"
-    private var isPlaceholderShown = true
-    private var typedText: String?
-    
-    private let textView: UITextView = {
-        let textView = UITextView()
-        textView.backgroundColor = .clear
-        textView.returnKeyType = .done
-        textView.autocapitalizationType = .sentences
-        textView.isScrollEnabled = true
-        textView.showsVerticalScrollIndicator = false
-        textView.autocorrectionType = .default
-        
-        let bodyFont = UIFont.preferredFont(forTextStyle: .body)
-        let scaledFontSize = bodyFont.pointSize * 1.1
-        let signikaBoldFont = UIFont(name: "Signika-Bold", size: scaledFontSize)
-        
-        textView.font = signikaBoldFont
-        
-        return textView
+
+    lazy var textView: UITextView = {
+       let tv = TextViewComponet()
+        tv.setupTextView()
+        return tv
     }()
     
     let contentBackGround: UIView = {
@@ -45,12 +29,6 @@ class TextView: UIView, UITextViewDelegate{
         super.init(frame: frame)
         
         self.backgroundColor = .white
-        textView.delegate = self
-        if typedText != nil {
-            isPlaceholderShown = false
-        }
-        textView.text = typedText ?? placeholderText
-        textView.textColor = .lightGray
         setupUI()
     }
     
@@ -62,33 +40,13 @@ class TextView: UIView, UITextViewDelegate{
         contentBackGround.centerX(inView: self)
         
         // Add textField to contentBackGround
-        contentBackGround.addSubview(textView)
+        self.addSubview(textView)
+        
         textView.anchorWithMultiplayerValues(top: contentBackGround.topAnchor, width: screenSize.width * 0.8, height: screenSize.height * 0.3)
         textView.centerX(inView: contentBackGround)
     }
     
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        if isPlaceholderShown {
-            textView.text = nil
-            textView.textColor = UIColor.darkPink()
-            isPlaceholderShown = false
-        }
-    }
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text.isEmpty {
-            textView.text = placeholderText
-            textView.textColor = .lightGray
-            isPlaceholderShown = true
-        }
-    }
-    func textViewDidChange(_ textView: UITextView) {
-        typedText = textView.text // Update typedText property when text changes
-    }
-    
-    func getTypedText() -> String? {
-        return typedText // Function to return the typed text
-    }
+   
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
